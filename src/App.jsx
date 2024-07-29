ca378d6b94fbe17a1008323934f08c61056507a7
Denzel-Santiago <233370@ids.upchiapas.edu.mx>
2024-07-28 19:34:19 -0600

    correcion del app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Conductor from './pages/Conductor';
 import Header from './components/molecules/Header';
@@ -12,7 +12,6 @@ import Administrador from './pages/Administrador';
 import Horario from './pages/Horario';
 import Urbans from './pages/Urbans';
 import Reporte from './pages/Reporte';
-import ReporteForm from './components/molecules/ReporteForm';
 import Tarifa from './pages/Tarifa';
 import Ganancias from './pages/Ganancias';
 import ReporteProblemas from './pages/ReporteProblemas';
@@ -27,19 +26,21 @@ import ReporteDescriptivo from './pages/ReporteDescriptivo';
 
 const PrivateRoute = ({ element, requiredRole }) => {
   const { isAuthenticated, role } = useAuth();
-
+  
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
   }
 
   if (requiredRole && role !== requiredRole) {
-    return <Navigate to="/unauthorized" />; // Puedes crear una página de "No autorizado"
+    return <Navigate to="/unauthorized" />;
   }
 
   return element;
 };
 
 function App() {
+  const location = useLocation();
+
   return (
     <AuthProvider>
       <NotificationProvider>
@@ -55,21 +56,20 @@ function App() {
             <Route path="/options" element={<PrivateRoute element={<ConductorOptions />} requiredRole={4} />} />
             <Route path='/optionschecador' element={<PrivateRoute element={<ChecadorOptions />} requiredRole={3} />} />
             <Route path='/optionsadmin' element={<PrivateRoute element={<Administrador />} requiredRole={1} />} />
-            <Route path='/Horario' element={<Horario />} />
-            <Route path='/Urbans' element={<Urbans />} />
-            <Route path='/Reporte' element={<Reporte />} />
-            <Route path='/ReporteForm' element={<ReporteForm />} />
-            <Route path='/Tarifa' element={<Tarifa />} />
-            <Route path='/Ganancias' element={<Ganancias />} />
-            <Route path='/ReporteProblemas' element={<ReporteProblemas />} />
-            <Route path='/ListaDeUnidadesPage' element={<ListaDeUnidadesPage />} />
-            <Route path='/CumplimientoDeHorarioPage' element={<CumplimientoDeHorario />} />
-            <Route path='/ChoferesPage' element={<ChoferesPage />} />
-            <Route path='/ListaDeChoferesPage' element={<ListaDeChoferesPage />} />
-            <Route path='/VerListas' element={<VerListas />} />
-            <Route path='/ReporteDescriptivo' element={<ReporteDescriptivo />} />
+            <Route path="/horario" element={<PrivateRoute element={<Horario />} requiredRole={1} />} />
+            <Route path="/admin/Urbans" element={<PrivateRoute element={<Urbans />} requiredRole={1} />} />
+            <Route path="/reporte" element={<PrivateRoute element={<Reporte />} requiredRole={1} />} />
+            <Route path="/tarifa" element={<PrivateRoute element={<Tarifa />} requiredRole={1} />} />
+            <Route path="/ganancias" element={<PrivateRoute element={<Ganancias />} requiredRole={4} />} />
+            <Route path="/reporte-problemas" element={<PrivateRoute element={<ReporteProblemas />} requiredRole={4} />} />
+            <Route path="/unidades" element={<PrivateRoute element={<ListaDeUnidadesPage />} requiredRole={1} />} />
+            <Route path="/cumplimiento-horario" element={<PrivateRoute element={<CumplimientoDeHorario />} requiredRole={3} />} />
+            <Route path="/admin/choferes" element={<PrivateRoute element={<ChoferesPage />} requiredRole={1} />} />
+            <Route path="/admin/listas" element={<PrivateRoute element={<ListaDeChoferesPage />} requiredRole={1} />} />
+            <Route path='/ver_listas'  element={<PrivateRoute element={<VerListas />} requiredRole={1} />} />
+            <Route path='/reporte-descriptivo' element={<PrivateRoute element={<ReporteDescriptivo />} requiredRole={4} />} />
           </Routes>
-          <Footer />
+          {location.pathname === '/' && <Footer />}
         </div>
       </NotificationProvider>
     </AuthProvider>
